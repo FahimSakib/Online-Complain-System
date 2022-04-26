1e888d9346ea937ac8f7f15516d174da238a9bfc
Istiak60 <istisquare@gmail.com>
2022-04-27 05:37:16 +0600

    HomePage_Without_js_css

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,12 +1,7 @@
-
-
-        
-
-      
 <!doctype html>
 <html lang="en">
 <head>
-    <base href="{{ asset('/') }}">
+    {{-- <base href="{{ asset('/') }}"> --}}
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="description" content="">
@@ -14,7 +9,7 @@
     <meta name="author" content="Ecology Theme">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Eduwise - Education HTML5 Template</title>
+    <title>Online-Complain-System | {{ $title }}</title>
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <!-- Goole Font -->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,500,700" rel="stylesheet">
@@ -58,78 +53,24 @@
     <div id="status">&nbsp;</div>
 </div>        
 
- @include('frontend.include.header')
+       @include('frontend.include.header')
  @include('frontend.include.login-signup-option')
 
 
+   @yield('content')
 
 
 
 
 
 
-<section class="features_4">
-    <div class="container">            
-        <div class="row res_mobile">
-            <div class="col-12 col-sm-12 col-md-8 col-lg-8">
-                <div class="features_wrapper_box">
-                    <div class="features_wrappe_single">
-                         <div class="single_item single_item_first">
-                            <div class="blog-img">
-                                <a href="#"><img src="images/features/features_1.jpg" alt="" class="img-fluid"></a>
-                            </div>
-                            <div class="blog_title">
-                                <span>look At</span>  
-                                <h3><a href="#" title="">Next Scheduled</a></h3> 
-                                <p>Lorem Ipsum dolor sit amet mollis felis arcu donec viverra. Pede phasellus etiam; Aaecenas vel vici quis dictum rutrum nec nisi et ac penatibus aenean....</p>                    
-                            </div>   
-                        </div>
-                        <div class="single_item single_item_last">
-                            <div class="blog-img">
-                                <a href="#" title=""><img src="images/features/features_3.jpg" alt="" class="img-fluid"></a>
-                            </div>
-                            <div class="blog_title">
-                                <span>Get Your</span>  
-                                <h3><a href="#" title="">Education Equip</a></h3> 
-                                 <p>Lorem Ipsum dolor sit amet mollis felis arcu donec viverra. Pede phasellus etiam; Aaecenas vel vici quis dictum rutrum nec nisi et ac penatibus aenean....</p>  
-                            </div>   
-                        </div>
-                    </div>
-                    <div class="features_wrappe_single">
-                        <div class="single_item single_item_center">
-                            <div class="blog-img">
-                                <a href="#" title=""><img src="images/features/features_2.jpg" alt="" class="img-fluid"></a>
-                            </div>
-                            <div class="blog_title">
-                                <span>Go To</span>  
-                                <h3><a href="#" title="">Online Courees</a></h3> 
-                                <p>Lorem Ipsum dolor sit amet mollis felis arcu donec viverra. Pede phasellus etiam; Aaecenas vel vici quis dictum rutrum nec nisi et ac penatibus aenean. Pede enim nunc ultricies quis rhoncus penatibus tincidunt integer felis...</p>                    
-                            </div>   
-                        </div>
-                    </div>
-                </div>
-            </div>             
-  
-            <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                <div class="features_4_content">
-                    <div class="title">
-                        <h2>Take The First Step To Learn With Us.</h2>
-                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos esentium voluptatum delenitamus et iusto odio dignissimos ducimus qui blanditiis pri atqueint. </p>
-                        <a href="#" title="">Learn More</a>
-                     </div>
-                </div>
-               
-            </div>            
-        </div>
-    </div>
-</section><!-- End Larnign Step -->
 
 
 
 
 
 
-<section class="popular_courses" id="popular_courses_4">
+{{-- <section class="popular_courses" id="popular_courses_4">
     <div class="container"> 
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-12">
@@ -568,7 +509,7 @@
     <div class="to-top pos-rtive">
         <a href="#"><i class = "flaticon-right-arrow"></i></a>
     </div>
-</section>
+</section> --}}
 
     
  @include('frontend.include.scripts')
