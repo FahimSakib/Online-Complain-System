879cc734b87b6f98d8b10e2848039ab16fc4e601
Istiak60 <istisquare@gmail.com>
2022-05-02 14:46:08 +0600

    mastering_for_complain_form

---
+++ resources/views/frontend/pages/complain/sections/form.blade.php
@@ -0,0 +1,85 @@
+<section class="contact_info_wrapper">
+    <div class="container">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="contact_form_wrapper">
+                <h3 class="title tex-center">Drop Your Complain</h3>
+                <div class="leave_comment">
+                    <div class="contact_form">
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-12 col-sm-12 col-md-6 form-group">
+                                    <label for="exampleFormControlSelect1">Department</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Departments</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+								   <div class="col-12 col-sm-12 col-md-6 form-group">
+                                    <label for="exampleFormControlSelect1">Teachers name</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Teachers Name</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+								  <div class="col-12 col-sm-12 col-md-6 form-group">
+                                    <label for="exampleFormControlSelect1">Late</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Teachers Name</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+								  <div class="col-12 col-sm-12 col-md-6 form-group">
+                                    <label for="exampleFormControlSelect1">Behaviour</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Teachers Name</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+								  <div class="col-12 col-sm-12 col-md-6 form-group">
+                                    <label for="exampleFormControlSelect1">Teaching method</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Teachers Name</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-6 form-group">
+                                    <label for="exampleFormControlSelect1">Marking</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Teachers Name</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                           
+                                <div class="col-12 col-sm-12 col-md-12 form-group">
+                                    <textarea class="form-control" id="comment"
+                                        placeholder="Your Comment Wite Here ..."></textarea>
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-12 submit-btn">
+                                    <button type="submit" class="text-center">Send Complain</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
