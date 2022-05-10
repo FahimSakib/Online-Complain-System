e4536849b8fcec11b050c1d0c3759f2574d1c2c8
Istiak60 <istisquare@gmail.com>
2022-05-11 03:25:13 +0600

    designed_updated

--- resources/views/frontend/pages/home/sections/testimonial.blade.php
+++ resources/views/frontend/pages/home/sections/testimonial.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-4">
                 <div class="testimonial_title">
-                    <h2>Student Say About Us.</h2>
+                    <h2>Facts of Complains.</h2>
                 </div>
             </div>
             <div class="col-12 col-sm-12 col-md-12 col-lg-8">
@@ -11,41 +11,41 @@
                     <div class="carousel_text slider-for">
                         <div>
                             <div class="single_box wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque um sociis natoque pena pretium quis, sem.</p>
+                                <p>People use languages to express their experience. However, each language is uniquely adapted for expressing only certain parts of our experience and is less effective for describing other parts</p>
                             </div>
                         </div>
                         <div>
                             <div class="single_box wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
-                                <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
+                                <p>There are very few people in this world with enough self-confidence, or self-importance, that they truly don't care what others think of them. Unless you're one of those people--and I'm certainly not--being late is bound to rattle you at least a little as you head into your meeting or appointment.</p>
                             </div>
                         </div>
                         <div>
                             <div class="single_box wow fadeInUp" data-wow-duration="2s" data-wow-delay=".4s">
-                                <p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.</p>
+                                <p>There are many ways to respond to inappropriate behaviors, of course, and they vary in how much they focus on the immediate behavior compared to longer-term features or patterns of a student’s behavior. There are so many ways to respond, in fact, that we can describe only a sample of the possibilities here. None are effective all of the time, though all do work at least some of the time.</p>
                             </div>
                         </div>
                     </div> 
                     <div class="reviewer_info">
                         <div class="carousel_asset/frontend/images slider-nav">
                             <div class="restimonial_single_img">
-                                <img src="asset/frontend/images/team/review_1.jpg" alt="2" class="img-fluid">
+                                <img src="asset/frontend/images/team/late11.jpg" alt="2" class="img-fluid">
                                 <div class="name_position">
-                                    <span class="name">Michael Jusi</span>
-                                    <span>Graphic Student</span>
+                                    <span class="name">Late</span>
+                                    {{-- <span>Graphic Student</span> --}}
                                 </div>
                             </div>                    
                             <div class="restimonial_single_img">
-                                <img src="asset/frontend/images/team/review_4.jpg" alt="2" class="img-fluid ">
+                                <img src="asset/frontend/images/team/behavior.jpg" alt="2" class="img-fluid ">
                                 <div class="name_position">
-                                    <span class="name">Jhon Smith</span>
-                                    <span>Web Student</span>
+                                    <span class="name">Behavior</span>
+                                    {{-- <span>Web Student</span> --}}
                                 </div>
                             </div>                    
                             <div class="restimonial_single_img ">
-                                <img src="asset/frontend/images/team/review_3.jpg" alt="2" class="img-fluid">
+                                <img src="asset/frontend/images/team/teacher.png" alt="2" class="img-fluid">
                                 <div class="name_position">
-                                    <span class="name">James Colins</span>
-                                    <span>Graphic Student</span>
+                                    <span class="name">Communication Skill</span>
+                                    
                                 </div>
                             </div>                    
                         </div>
