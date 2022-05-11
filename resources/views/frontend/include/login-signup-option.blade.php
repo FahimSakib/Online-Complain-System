56ef02838956d4882c2f448c91b693b93c755764
Istiak60 <istisquare@gmail.com>
2022-05-11 16:56:44 +0600

    modal_updated

--- resources/views/frontend/include/login-signup-option.blade.php
+++ resources/views/frontend/include/login-signup-option.blade.php
@@ -8,9 +8,11 @@
                 <div class="row">
                     <div class="col-12 col-lg-12 col-md-12 col-lg-12 login_option_btn">
                         <ul class="nav nav-tabs" role="tablist">
-                            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#login" role="tab">Login</a></li>
-                            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#panel2" role="tab">Register</a></li>
-                           
+                            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#login"
+                                    role="tab">Login</a></li>
+                            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#panel2"
+                                    role="tab">Register</a></li>
+
                         </ul>
                     </div>
                     <div class="col-12 col-lg-12 col-md-12 col-lg-12">
@@ -18,74 +20,143 @@
                         <div class="tab-content card">
                             <!--Login-->
                             <div class="tab-pane fade in show active" id="login" role="tabpanel">
-                                <form action="#">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
                                     <div class="row">
                                         <div class="col-12 col-lg-12 col-md-12 col-lg-12">
+                                      
                                             <div class="form-group">
-                                                <label class="control-label">Email</label>
-                                                <input type="email" class="form-control" placeholder="Username">
-                                            </div>
+                                                <label for="control-label" value="{{ __('Email') }}">Email address
+                                                    *</label>
+                                                <input type="text" class="form-control" id="email" type="email"
+                                                    name="email" :value="old('email')" required autofocus
+                                                    placeholder="Email">
+                                            </div><!-- End .form-group -->
+
                                         </div>
+
                                         <div class="col-12 col-lg-12 col-md-12 col-lg-12">
+                                         
                                             <div class="form-group">
-                                                <label class="control-label">Password</label>
-                                                <input type="password" class="form-control" placeholder="Password">
-                                            </div>
+                                                <label for="password" value="{{ __('Password') }}">Password *</label>
+                                                <input type="password" id="password" class="form-control"
+                                                    name="password" required autocomplete="current-password"
+                                                    placeholder="Password">
+                                            </div><!-- End .form-group -->
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-12 col-lg-12 col-md-12 col-lg-12 d-flex justify-content-between login_option">
-                                            <a href="forgot-password.html" title="" class="forget_pass">Forget Password ?</a>
-                                            <button type="submit" class="btn btn-default login_btn">Login</button>
-                                        </div> 
-                                        <div class="col-12 col-lg-12 col-md-12 col-lg-12">
-                                            <div class="social_login">
-                                                <div class="social_items">
-                                                    <button class="google_login google">Login Google</button>
-                                                    <button class="google_login facebook">Login Facebook</button>
-                                                    <button class="google_login twitter">Login Twitter</button>
-                                                    <button class="google_login linkdin">Login Linkdin</button>
-                                                </div>
-                                            </div>
+                                        <div
+                                            class="col-12 col-lg-12 col-md-12 col-lg-12 d-flex justify-content-between login_option">
+                                           
+                                            @if (Route::has('password.request'))
+                                            <a class=" forgot-link forget_pass" href="{{ route('password.request') }}">
+                                                {{ __('Forgot your password?') }}
+                                            </a>
+                                            @endif
+                                            <button type="submit" class="btn btn-default login_btn">
+                                                <span> {{ __('Log in') }}</span>
+                                                <i class="icon-long-arrow-right"></i>
+                                            </button>
                                         </div>
+
                                     </div>
                                 </form>
                             </div>
                             <!--/.Panel 1-->
                             <!--Panel 2-->
                             <div class="tab-pane fade" id="panel2" role="tabpanel">
-                                <form action="#" class="register">
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
                                     <div class="row">
                                         <div class="col-12 col-lg-12 col-md-12 col-lg-12">
+
+                                            {{-- FOR NAME --}}
                                             <div class="form-group">
-                                                <label  class="control-label">Name</label>
-                                                <input type="text" class="form-control" placeholder="Username">
-                                            </div>
-                                        </div>                                        
-                                        <div class="col-12 col-lg-12 col-md-12 col-lg-12">
-                                            <div class="form-group">
-                                                <label  class="control-label">Email</label>
-                                                <input type="email" class="form-control" placeholder="Email">
-                                            </div>
+                                                <label for="register-name control-label" value="{{ __('Name') }}">User
+                                                    Name *</label>
+                                                <input type="text" class="form-control" id="ragister-name" name="name"
+                                                    :value="old('name')" required autofocus autocomplete="name"
+                                                    placeholder="Username">
+                                            </div><!-- End .form-group -->
                                         </div>
                                         <div class="col-12 col-lg-12 col-md-12 col-lg-12">
+
+                                            {{-- FOR Email --}}
                                             <div class="form-group">
-                                                <label  class="control-label">Password</label>
-                                                <input type="password" class="form-control" placeholder="Password">
+                                                <label for="email control-label" value="{{ __('Email') }}">Your Email
+                                                    *</label>
+                                                <input type="email" class="form-control" id="ragister-email"
+                                                    name="email" :value="old('email')" required placeholder="Email">
+                                            </div><!-- End .form-group -->
+
+                                        </div>
+
+                                        <div class="col-12 col-lg-12 col-md-12 col-lg-12">
+                                            {{-- FOR Password --}}
+                                            <div class="form-group">
+                                                <label for="password" value="{{ __('Password') }}">Password *</label>
+                                                <input type="password" class="form-control" id="register-password"
+                                                    type="password" name="password" required autocomplete="new-password"
+                                                    placeholder="Password">
+                                            </div><!-- End .form-group -->
+                                        </div>
+                                        <div class="col-12 col-lg-12 col-md-12 col-lg-12">
+                                            {{-- FOR  Confirm Password --}}
+                                            <div class="form-group">
+                                                <label for="password_confirmation"
+                                                    value="{{ __('Confirm Password') }}">Confirm Password *</label>
+                                                <input type="password" id="password_confirmation" class="form-control"
+                                                    type="password" name="password_confirmation" required
+                                                    autocomplete="new-password" placeholder="Confirm Password">
+                                            </div><!-- End .form-group -->
+                                        </div>
+
+                                        <div class="row">
+                                            <div
+                                                class="col-12 col-lg-12 col-md-12 col-lg-12 d-flex justify-content-between login_option">
+                                                @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                                                <div class="mt-4">
+                                                    <x-jet-label for="terms">
+                                                        <div class="flex items-center">
+                                                            <x-jet-checkbox name="terms" id="terms" />
+                                                            <div class="ml-2">
+                                                                {!! __('I agree to the :terms_of_service and
+                                                                :privacy_policy', [
+                                                                'terms_of_service' => '<a target="_blank"
+                                                                    href="'.route('terms.show').'"
+                                                                    class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms
+                                                                    of Service').'</a>',
+                                                                'privacy_policy' => '<a target="_blank"
+                                                                    href="'.route('policy.show').'"
+                                                                    class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy
+                                                                    Policy').'</a>',
+                                                                ]) !!}
+                                                            </div>
+                                                        </div>
+                                                    </x-jet-label>
+                                                </div>
+                                                @endif
+                                                <div class="form-footer">
+
+
+                                        
+                                                    <button type="submit" class="btn btn-default login_btn">
+                                                        <span> {{ __('Register') }}</span>
+                                                        <i class="icon-long-arrow-right"></i>
+                                                    </button>
+                                                </div><!-- End .form-footer -->
+
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-12 col-lg-12 col-md-12 col-lg-12 d-flex justify-content-between login_option">
-                                            <button type="submit" class="btn btn-default login_btn">Register</button>
-                                        </div> 
-                                    </div>
+                                        </div>
                                 </form>
-                            </div><!--/.Panel 2-->
+                            </div>
+                            <!--/.Panel 2-->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</section>  <!-- End Login Signup Option -->
+</section> <!-- End Login Signup Option -->
